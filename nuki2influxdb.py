e13cd28a89b94a0a92b934bae969e2e712eb028b
dbsqp <5895806+dbsqp@users.noreply.github.com>
2022-04-10 15:47:12 +0200

    Update nuki2influxdb.py

--- nuki2influxdb.py
+++ nuki2influxdb.py
@@ -32,7 +32,7 @@ else:
 
 
 # Nuki envionment variables
-nuki_ip_bridge=os.getenv('NUKI_BRIDGE', "")
+nuki_bridge=os.getenv('NUKI_BRIDGE', "")
 nuki_token=os.getenv('NUKI_TOKEN', "")
 nuki_opener_list_str=os.getenv('NUKI_OPENER_LIST', "")
 nuki_lock_list_str=os.getenv('NUKI_LOCK_LIST', "")
@@ -66,109 +66,59 @@ if debug:
 client = InfluxDBClient(url=influxdb2_url, token=influxdb2_token, org=influxdb2_org)
 write_api = client.write_api(write_options=SYNCHRONOUS)
 
-
-# fixed sensors
-sensorList=['Temperature','Humidity']
-
-
 # pass data to InfluxDB
-for ipaddress in homekit_ip_list:
-	senddata={}
 
-	time = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ") 
+time = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ") 
 
-	# get host name
-	position = homekit_ip_list.index(ipaddress)
-	sensorName=homekit_sensorName_list[position]
+# test if bridge responde to ping
+if pingTest(nuki_bridge):
 	if debug:
-		print ("\nSensor: "+sensorName+" - "+ipaddress)
-
-	# test if host responde to ping
-	if pingTest(ipaddress):
-		if debug:
-			print ("  PING: OK")
-	else:
-		if debug:
-			print ("  PING: NOK")
-		continue
-
-	# get REST API
-	url="http://"+ipaddress+":5556/"
-	try:
-		raw = requests.get(url, timeout=4)
-	except requests.exceptions.Timeout as e: 
-		if debug:
-			print ("   API:",e)
-		continue
-
-	if raw.status_code == requests.codes.ok:
-		if debug:
-			print ("   API: OK ["+str(raw.status_code)+"]")
-		ds = raw.json()
-		if debug and showraw:
-			print ("   RAW:")
-			print (json.dumps(ds,indent=4))
-	else:
-		if debug:
-			print ("   API: NOK")
-		continue
-
-	# get MAC
-	if getmac:
-		mac = get_mac_address(ip=ipaddress)
-	else:
-		mac=homekit_mac_list[position]	
+		print ("  PING: OK")
+else:
 	if debug:
-		print ("   MAC: "+mac)
+		print ("  PING: NOK")
+continue
+
+# get REST API
+url="http://"+nuki_bridge+":8080/list&token="+nuki_token
+try:
+	raw = requests.get(url, timeout=4)
+except requests.exceptions.Timeout as e: 
+	if debug:
+		print ("   API:",e)
+	continue
+
+if raw.status_code == requests.codes.ok:
+	if debug:
+		print ("   API: OK ["+str(raw.status_code)+"]")
+	ds = raw.json()
+	if debug and showraw:
+		print ("   RAW:")
+		print (json.dumps(ds,indent=4))
+else:
+	if debug:
+		print ("   API: NOK")
+	continue
+
+
        
-	# get sensor values
-	for sensor in [0,1]:
-		value=ds['accessories'][1]['services'][sensor+1]['characteristics'][0]['value']
+# get sensor values
+senddata={}
+
+value=ds['accessories'][1]['services'][sensor+1]['characteristics'][0]['value']
+value=float(round(value,1))
         	
-		senddata["measurement"]=sensorList[sensor].lower()
-#		senddata["time"]=time
-		senddata["tags"]={}
-		senddata["tags"]["origin"]="HomeKit"
-		senddata["tags"]["source"]="docker homekit-influxdb2"
-		senddata["tags"]["sensor"]=sensorName
-		senddata["tags"]["hardware"]=mac
-		senddata["fields"]={}
+senddata["measurement"]="battery"
+senddata["tags"]={}
+senddata["tags"]["origin"]="Nuki"
+senddata["tags"]["source"]="docker nuki-influxdb2"
+senddata["tags"]["host"]="Opener"
+senddata["fields"]={}
+senddata["fields"]["percent"]=value
 		
-		if sensorList[sensor] == "Temperature":
-			value=float(round(value,1))
-			senddata["fields"]["temp"]=value
-		else:
-			value=int(value)
-			senddata["fields"]["percent"]=value
-		
-		if debug:
-			print ("INFLUX: "+influxdb2_bucket)
-			print (json.dumps(senddata,indent=4))
-		write_api.write(bucket=influxdb2_bucket, org=influxdb2_org, record=[senddata])
+if debug:
+	print ("INFLUX: "+influxdb2_bucket)
+	print (json.dumps(senddata,indent=4))
+write_api.write(bucket=influxdb2_bucket, org=influxdb2_org, record=[senddata])
 
-	# do additional temperature sensors
-	if homekit_add_list[position] != "":
-		if debug:
-			print ("ADD: "+homekit_add_list[position][0]+" - "+homekit_add_list[position][1])
-		value=ds['accessories'][2]['services'][1]['characteristics'][0]['value']
-		if homekit_add_list[position][1] == "Temperature":
-			value=float(round(value,1))
-		else:
-			value=int(value)
 
-		senddata={}
-		senddata["measurement"]=homekit_add_list[position][1].lower()
-		senddata["tags"]={}
-		senddata["tags"]["source"]="docker homekit-influxdbv2"
-		senddata["tags"]["origin"]="HomeKit"
-		senddata["tags"]["sensor"]=homekit_add_list[position][0]
-		senddata["tags"]["hardware"]=mac
-		senddata["fields"]={}
-		senddata["fields"]["temp"]=value
-		if debug:
-			print ("INFLUX: "+influxdb2_bucket)
-			print (json.dumps(senddata,indent=4))
-		write_api.write(bucket=influxdb2_bucket, org=influxdb2_org, record=[senddata])
-	else:
-		if debug:
-			print ("ADD: NULL")
