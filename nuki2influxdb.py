d8cd90f1f5d5f5baedac616dccae06224cc4ba59
dbsqp <5895806+dbsqp@users.noreply.github.com>
2022-04-10 15:35:33 +0200

    Update nuki2influxdb.py

--- nuki2influxdb.py
+++ nuki2influxdb.py
@@ -32,16 +32,13 @@ else:
 
 
 # Nuki envionment variables
-homekit_ip_list_str=os.getenv('HOMEKIT_IP_LIST', "")
-homekit_sensorName_list_str=os.getenv('HOMEKIT_SENSOR_LIST', "")
-homekit_mac_list_str=os.getenv('HOMEKIT_MAC_LIST', "")
-homekit_add_list_str=os.getenv('HOMEKIT_ADD_LIST', "")
+nuki_ip_bridge=os.getenv('NUKI_BRIDGE', "")
+nuki_token=os.getenv('NUKI_TOKEN', "")
+nuki_opener_list_str=os.getenv('NUKI_OPENER_LIST', "")
+nuki_lock_list_str=os.getenv('NUKI_LOCK_LIST', "")
 
-homekit_ip_list=eval(homekit_ip_list_str)
-homekit_sensorName_list=eval(homekit_sensorName_list_str)
-homekit_mac_list=eval(homekit_mac_list_str)
-homekit_add_list=eval(homekit_add_list_str)
-
+nuki_opener_list=eval(nuki_opener_list_str)
+nuki_lock_list=eval(nuki_lock_list_str)
 
 # influxDBv2 envionment variables
 influxdb2_host=os.getenv('INFLUXDB2_HOST', "localhost")
